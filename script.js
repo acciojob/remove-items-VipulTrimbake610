b82c5ad650632c37e901f7df20de2440ef721be4
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-20 12:00:33 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const select = document.getElementById("colorSelect");
+const btn  = document.getElementsByTagName("input")[0];
+
+btn.addEventListener("click",function(){
+	console.log("Hello")
+	let arr = select.children; 
+	for(let i=0;i<arr.length;i++){
+		if(arr[i].value === select.value){
+			arr[i].remove();
+		}
+	}
+console.log(select.children);
+});
